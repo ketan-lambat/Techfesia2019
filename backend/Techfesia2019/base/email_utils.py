0a266864be1238c635bf7e1d3942000ea1da792e
Hrishabh Pandey <44145550+hrs2203@users.noreply.github.com>
2019-06-28 09:57:29 +0530

    Add files via upload

--- backend/Techfesia2019/base/email_utils.py
+++ backend/Techfesia2019/base/email_utils.py
@@ -1,1 +1,11 @@
 # will contain all email utils
+from django.core.mail import send_mail
+from django.conf import settings
+
+def welcomeEmail():
+    subject = "welcome email"
+    from_email = "hrishabh2203@gmail.com"
+    to_email = [ 'hrishabh.p18@iiit.in' ]
+
+    content_message = "welcome people this is working"
+    send_mail(subject,content_message,from_email,to_email,fail_silently=True)
