e3dfee812fc78a1faf1b5b82e186182721f9dd5c
ketan-lambat <ketan.lambat@ymail.com>
2019-06-29 16:33:59 +0530

    Html template in email working

--- backend/Techfesia2019/base/email_utils.py
+++ backend/Techfesia2019/base/email_utils.py
@@ -1,5 +1,8 @@
 # will contain all email utils
+from django.core import mail
 from django.core.mail import send_mail
+from django.template.loader import render_to_string
+from django.utils.html import strip_tags
 from django.conf import settings
 
 def welcomeEmail():
@@ -12,9 +15,13 @@ def welcomeEmail():
 
 def welcomeEmailhtml():
     subject = "welcome email (html)"
+    html_message = render_to_string('emails/welcome.html', {'context': 'values'})
+    plain_message = strip_tags(html_message)
     from_email = "lambatketan@gmail.com"
     to_email = ['ketan.lambat@ymail.com']
 
     content_message = "email visible in browser"
-    send_mail(subject, content_message, from_email, to_email, fail_silently=False)
+    mail.send_mail(subject, plain_message, from_email, [to_email], html_message=html_message)
+   # send_mail(subject, content_message, from_email, to_email, fail_silently=False)
 
+    # https://stackoverflow.com/questions/3005080/how-to-send-html-email-with-django-with-dynamic-content-in-it
