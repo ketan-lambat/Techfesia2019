cf46e76109904bd77122da1d27b4c793c71001b1
ketan-lambat <ketan.lambat@ymail.com>
2019-07-11 16:00:48 +0530

    test commit 1

--- backend/Techfesia2019/tickets/tests.py
+++ backend/Techfesia2019/tickets/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
+from django.test import SimpleTestCase
+from django.urls  import reverse, resolve
+from .views import TicketCreateListView
 
 # Create your tests here.
+
+class TestURLs(SimpleTestCase):
+  def test_url_is_resolved(self):
+    url = reverse('TicketCreate')
+    print(resolve(url))
+    self.assertEquals(resolve(url).func, TicketCreateListView)
--- backend/Techfesia2019/tickets/urls.py
+++ backend/Techfesia2019/tickets/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
+
 from .views import TicketCreateListView, TicketCloseView, PublicTicketListView, TicketCommentDetailUpdateDeleteView
 from .views import TicketCommentListCreateView
 
 urlpatterns = [
-    path('', TicketCreateListView.as_view()),
+    path('', TicketCreateListView.as_view(), name="TicketCreate"),
     path('public/', PublicTicketListView.as_view()),
     path('<str:username>/', TicketCreateListView.as_view()),
     path('<str:public_id>/close/', TicketCloseView.as_view()),
