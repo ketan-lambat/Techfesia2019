bc2e338eb4b78d7cbd5e0ef8486753c6427db18b
ketan-lambat <ketan.lambat@ymail.com>
2019-07-11 16:21:32 +0530

    name added in urls.py

--- backend/Techfesia2019/base/urls.py
+++ backend/Techfesia2019/base/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     #path('', views.email_welcome),
-    path('', views.email_test),
-    path('welcome/', views.email_welcome),
+    path('', views.email_test, name="test_email"),
+    path('welcome/', views.email_welcome, name="welcome_email"),
 ]
