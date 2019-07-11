2e4d5e2d336c9a3cb9b70bc2af1e1be7ba06a34d
ketan-lambat <ketan.lambat@ymail.com>
2019-07-11 16:06:37 +0530

    faulty test removed

--- backend/Techfesia2019/tickets/tests.py
+++ backend/Techfesia2019/tickets/tests.py
@@ -5,8 +5,3 @@ from .views import TicketCreateListView
 
 # Create your tests here.
 
-class TestURLs(SimpleTestCase):
-  def test_url_is_resolved(self):
-    url = reverse('TicketCreate')
-    print(resolve(url))
-    self.assertEquals(resolve(url).func, TicketCreateListView)
